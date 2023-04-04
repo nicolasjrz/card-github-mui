d0a9fb89803d194e5956e70f381a2d4efbcedd7c
nicolasjrz <nicolasjrz.dev@gmail.com>
2023-04-04 17:32:58 -0300

    create <PaperInformation/> and <LocationInformation/>

--- src/components/card/DescriptionCard.jsx
+++ src/components/card/DescriptionCard.jsx
@@ -1,5 +1,7 @@
 import { Stack, Typography } from "@mui/material";
 import React from "react";
+import { PaperInformation } from "./PaperInformation";
+import { LocationInformation } from "./LocationInformation";
 
 export const DescriptionCard = () => {
   return (
@@ -10,6 +12,9 @@ export const DescriptionCard = () => {
           nihil ratione odit, consequuntur possimus ipsam alias at quis quas
           libero optio ipsa illum! Atque voluptatibus nemo in itaque eum.
         </Typography>
+
+        <PaperInformation />
+        <LocationInformation />
       </Stack>
     </>
   );
---
+++ src/components/card/PaperInformation.jsx
@@ -0,0 +1,28 @@
+import { Paper, Stack, Typography } from "@mui/material";
+import React from "react";
+
+export const PaperInformation = () => {
+  return (
+    <>
+      <Paper elevation={3} sx={{ marginTop: "15px" }}>
+        <Stack
+          direction={"row"}
+          sx={{ justifyContent: "space-evenly", margin: "20px" }}
+        >
+          <Stack>
+            <Typography variant="h5">public repositories</Typography>
+            <Typography variant="h6">1</Typography>
+          </Stack>
+          <Stack>
+            <Typography variant="h5">followers</Typography>
+            <Typography variant="h6">1</Typography>
+          </Stack>
+          <Stack>
+            <Typography variant="h5">following</Typography>
+            <Typography variant="h6">1</Typography>
+          </Stack>
+        </Stack>
+      </Paper>
+    </>
+  );
+};
